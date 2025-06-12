e008ca008d5930456178cdbe27ab072e8837c88c
Baum <sometree@sometree.dev>
2025-06-12 13:19:16 +0200

    m when I forget to save

--- nixconfig/default.nix
+++ nixconfig/default.nix
@@ -11,26 +11,7 @@
       ./programs.nix
     ];
 
-  boot = {
-    loader = {
-      efi.canTouchEfiVariables = true;
-      grub = {
-        enable = true;
-        efiSupport = true;
-        device = "nodev";
-      };
-    };
-    initrd = {
-      kernelModules = ["usb_storage"];
-      luks.devices.cryptroot = {
-        device = "/dev/nvme0n1p2";
-        preLVM = true;
-      };
-    };
-  };
-
   networking = {
-    hostName = "baum-surface-nix";
     networkmanager = {
       enable = true;
     };
