87cc51505ed12fbcdfa4b7b341ad300e7de81c60
Baum <sometree@sometree.dev>
2025-06-12 12:10:30 +0200

    seperate hardware config hopefully

--- flake.nix
+++ flake.nix
@@ -3,26 +3,55 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    hyprland.url = "github:hyprwm/Hyprland";
+    catppuccin.url = "github:catppuccin/nix";
+    comic-code-ligatures-nerd-font = {
+      url = "github:juliuskreutz/ComicCodeLigaturesNerdFont";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs =  {self, nixpkgs, home-manager, hyprland, ...}@inputs: {
-    nixosConfigurations = {
-      baum-surface-nix = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./nixconfig
-          ./hardware-configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.baum = ./home;
-          }
-        ];
+  outputs = inputs: {
+    nixosConfigurations =
+      let
+        nixosSystem =
+          surface:
+          let
+            hostName = if surface then "baum-surface-nix" else "baum-desktop-nix";
+            hardwareConfiguration =
+              if surface then ./hosts/surface.nix else ./hosts/desktop.nix;
+          in
+          inputs.nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = { inherit inputs surface; };
+            modules = [
+              { networking.hostName = hostName; }
+              hardwareConfiguration
+              ./nixconfig
+              inputs.home-manager.nixosModules.home-manager
+              {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  useUserPackages = true;
+                  extraSpecialArgs = {
+                    inherit inputs;
+                  };
+                  users.baum = {
+                    imports = [
+                      ./home
+                    ];
+                  };
+                };
+              }
+            ];
+          };
+      in
+      {
+        nix-desktop = nixosSystem false;
+        nix-surface = nixosSystem true;
       };
-    };
   };
 }
