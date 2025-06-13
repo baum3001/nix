5be8ac87a6b02fe3f6063758730e5d1086ab2111
Baum <sometree@sometree.dev>
2025-06-13 19:36:37 +0200

    schboddify

--- home/programs.nix
+++ home/programs.nix
@@ -85,6 +85,7 @@
     signal-desktop
     kdePackages.dolphin
     inputs.comic-code-ligatures-nerd-font.packages.x86_64-linux.comic-code-ligatures-nerd-font
+    spotify-tui
   ];
 
   services = {
