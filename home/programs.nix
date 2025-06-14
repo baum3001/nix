34ecffb97ac443440fa6df529bea0446a399ce82
Baum <sometree@sometree.dev>
2025-06-14 14:02:01 +0200

    librewolf config

--- home/programs.nix
+++ home/programs.nix
@@ -42,6 +42,13 @@
     };
     librewolf = {
       enable = true;
+      settings = {
+        "webgl.disabled" = false;
+        "privacy.resistFingerprinting" = false;
+        "privacy.clearOnShutdown.history" = false;
+        "privacy.clearOnShutdown.cookies" = false;
+        "network.cookie.lifetimePolicy" = 0;
+      };
     };
     waybar = {
       enable = true;
