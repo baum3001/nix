63ac1402b70f947e9880b32801f42c147826edc8
Baum <sometree@sometree.dev>
2025-06-15 16:02:24 +0200

    me when I forget staging changes

--- home/default.nix
+++ home/default.nix
@@ -8,11 +8,6 @@
     inputs.catppuccin.homeModules.catppuccin
   ];
 
-  
-  home.file.".config/quickshell" = {
-    source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/home/ui/quickshell/config";
-  };
-
   home.username = "baum";
   home.homeDirectory = "/home/baum";
   home.stateVersion = "25.05";
