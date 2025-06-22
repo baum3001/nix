3905194a28dec322561b92564448f9d95c6ef92d
Baum <sometree@sometree.dev>
2025-06-22 17:21:44 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHhh

--- home/programs/librewolf.nix
+++ home/programs/librewolf.nix
@@ -2,17 +2,53 @@
 
 
 {
-  programs.librewolf = {
+  programs.firefox = {
     enable = true;
-    settings = {
-      "webgl.disabled" = false;
-      "privacy.resistFingerprinting" = false;
-      "privacy.clearOnShutdown.history" = false;
-      "privacy.clearOnShutdown.cookies" = false;
-      "network.cookie.lifetimePolicy" = 0;
-      "privacy.clearSiteData.cookiesAndStorage" = false;
-      "privacy.clearOnShutdown_v2.cookiesAndStorage" = false;
-      "privacy.clearHistory.cookiesAndStorage" = false;
+    package = pkgs.librewolf;
+    policies = {
+
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      ExtensionSettings = {
+        # Catpuccin Mocha - Lavender
+        "{8446b178-c865-4f5c-8ccc-1d7887811ae3}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/catppuccin-mocha-lavender-git/latest.xpi";
+          installation_mode = "force_installed";
+	      };
+        # uBlock Origin
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+      FirefoxHome = {
+        "Search" = false;
+      };
+      HardwareAcceleration = true;
+      Preferences = {
+        "browser.preferences.defaultPerformanceSettings.enabled" = false;
+        "browser.startup.homepage" = "about:home";
+        "browser.toolbar.bookmarks.visibility" = "newtab";
+        "browser.toolbars.bookmarks.visibility" = "newtab";
+        "browser.urlbar.suggest.bookmark" = false;
+        "browser.urlbar.suggest.engines" = false;
+        "browser.urlbar.suggest.history" = true;
+        "browser.urlbar.suggest.openpage" = false;
+        "browser.urlbar.suggest.recentsearches" = false;
+        "browser.urlbar.suggest.topsites" = false;
+	      "browser.warnOnQuit" = false;
+        "browser.warnOnQuitShortcut" = false;
+        "webgl.disabled" = false;
+        "privacy.sanitize.sanitizeOnShutdown" = false;
+        "privacy.resistFingerprinting" = false;
+        "privacy.clearOnShutdown.history" = false;
+        "privacy.clearOnShutdown.cookies" = false;
+        "network.cookie.lifetimePolicy" = 0;
+        "privacy.clearSiteData.cookiesAndStorage" = false;
+        "privacy.clearOnShutdown_v2.cookiesAndStorage" = false;
+        "privacy.clearHistory.cookiesAndStorage" = false;
+
+      };
     };
   };
 }
