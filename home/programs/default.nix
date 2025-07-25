a253dd9d1694e32c3e407bd73b5c1c212f75fb8a
Baum <sometree@sometree.dev>
2025-07-26 01:20:27 +0200

    make fish default kitty shell

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -66,6 +66,8 @@
       enable = true;
       settings = {
         confirm_os_window_close = 0;
+        font_size = 13;
+        shell = "fish";
         dynamic_background_opacity = true;
         enable_audio_bell = false;
         mouse_hide_wait = "-1.0";
