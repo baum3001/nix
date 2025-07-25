3e4d12627e939768b7a43ea82b2c3cf6859215db
Baum <sometree@sometree.dev>
2025-07-26 01:39:44 +0200

    first starship experiments, needs font

--- home/programs/shell.nix
+++ home/programs/shell.nix
@@ -3,7 +3,6 @@
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
-      set fish_greeting # Disable greeting
     '';
     plugins = [
       # Enable a plugin (here grc for colorized command output) from nixpkgs
@@ -11,5 +10,19 @@
       # Manually packaging and enable a plugin
       
     ];
+  };
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      add_newline = true;
+      command_timeout = 1300;
+      scan_timeout = 50;
+      format = "$all$nix_shell$nodejs$lua$golang$rust$php$git_branch$git_commit$git_state$git_status\n$username$hostname$directory";
+      character = {
+        success_symbol = "[](bold green) ";
+        error_symbol = "[✗](bold red) ";
+      };
+    };
   };
 }
