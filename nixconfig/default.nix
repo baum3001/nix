b9d491070e26d31050c498e6ab223ce56fbf30f9
Baum <sometree@sometree.dev>
2025-08-03 19:45:21 +0200

    update wireguard config and move it to my user config

--- nixconfig/default.nix
+++ nixconfig/default.nix
@@ -66,20 +66,6 @@
   "steam-unwrapped"
   "steam-run"
   ];
-  networking.firewall = {
-   # if packets are still dropped, they will show up in dmesg
-   logReversePathDrops = true;
-   # wireguard trips rpfilter up
-   extraCommands = ''
-     ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 3700 -j RETURN
-     ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 3700 -j RETURN
-   '';
-   extraStopCommands = ''
-     ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 3700 -j RETURN || true
-     ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 3700 -j RETURN || true
-   '';
-  };
-
 
   system.stateVersion = "25.05"; # Did you read the comment?
 
