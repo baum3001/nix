28e9279b8db5e70c939a06c53889108dff3ab42d
Baum <sometree@sometree.dev>
2025-08-03 19:34:57 +0200

    refactor things for more future proof

--- homeModules/spicetify.nix
+++ homeModules/spicetify.nix
@@ -6,9 +6,7 @@ imports = [
   inputs.spicetify-nix.homeManagerModules.default
 
 ];
- nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-  "spotify"
-  ];
+
 programs.spicetify =
 let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
