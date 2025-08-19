352f4ed31bb7ce9877ef0263bee7181ec89dd514
Baum <sometree@sometree.dev>
2025-08-19 20:59:27 +0200

    it no longer jiggles

--- homeModules/caelestia.nix
+++ homeModules/caelestia.nix
@@ -126,7 +126,7 @@
       "caelestia shell -d"
     ];
     general = {
-      gaps_in = "0";
+      gaps_in = "1";
       gaps_out = "0";
       border_size = "0";
       # https://wiki.hyprland.org/Configuring/Variables/#variable-types for info about colors
